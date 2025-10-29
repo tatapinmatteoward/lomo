d7a5bf65ea7707e0b086980f6091d66397eb2999
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 10:50:09 +0000

    Add .vitepress/theme/posts.data.js

---
+++ .vitepress/theme/posts.data.js
@@ -0,0 +1,45 @@
+export default {
+  load() {
+    return [
+    {
+        "url": "/blog/natural-remedies-for-anxiety-step-by-step-holistic-solutions",
+        "title": "Natural Remedies for Anxiety: Step-by-Step Holistic Solutions",
+        "excerpt": "Discover effective, natural ways to manage anxiety through holistic approaches like herbs, mindfulness, and lifestyle changes. Empower your mental health journey.",
+        "date": "2025-10-29",
+        "tags": [
+            "anxiety relief",
+            "holistic remedies",
+            "natural healthcare",
+            "mindfulness",
+            "herbal medicine"
+        ]
+    },
+    {
+        "url": "/blog/boosting-immunity-with-natures-pharmacy-essential-oils-and-fruits",
+        "title": "Boosting Immunity with Nature's Pharmacy: Essential Oils and Fruits",
+        "excerpt": "Discover natural ways to strengthen your immune system using essential oils and nutrient-rich fruits from nature's bounty.",
+        "date": "2023-10-15",
+        "tags": [
+            "health",
+            "immunity",
+            "essential-oils",
+            "fruits",
+            "natural-remedies"
+        ]
+    },
+    {
+        "url": "/blog/holistic-detoxification-cleansing-your-body-with-herbal-teas-and-juices",
+        "title": "Holistic Detoxification: Cleansing Your Body with Herbal Teas and Juices",
+        "excerpt": "Discover natural ways to detoxify your body using herbal teas and juices, promoting holistic health and vitality through simple, effective methods.",
+        "date": "2023-10-15",
+        "tags": [
+            "detox",
+            "herbal-teas",
+            "juices",
+            "holistic-health",
+            "natural-healing"
+        ]
+    }
+]
+  }
+}
